fe1f949a4f9f713c63e1afa86a418365825abd97
niquolic <nicolas.debruycker22@gmail.com>
2025-01-05 21:48:42 +0100

    return benzema gif

--- commands/utility/benzema.js
+++ commands/utility/benzema.js
@@ -5,6 +5,11 @@ module.exports = {
         .setName('benzema')
         .setDescription("Return a Benzema's GIF"),
     async execute(interaction) {
-        await interaction.reply('Benzegoal')
+        const url = `http://api.giphy.com/v1/gifs/search?q=benzema
+        &api_key=${process.env.GIPHY_API_KEY}&limit=100`
+        const res = await fetch(url)
+        const json = await res.json()
+        const randomIndex = Math.floor(Math.random() * json.data.length)
+        await interaction.reply(json.data[randomIndex].url);
     }
 }
